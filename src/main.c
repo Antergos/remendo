a04be97d7974f462af5a728292d277ece7fe06ae
Alexandre Filgueira <faidoc@gmail.com>
2013-06-06 15:43:49 +0200

    Common C project files

--- src/main.c
+++ src/main.c
@@ -1,12 +1,21 @@
-/*************************************************************************\
-*                  Alexandre Filgueira - Antergos                         *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU Affero General Public License as published   *
-* by the Free Software Foundation, either version 3 or (at your option)   *
-* any later version. This program is distributed without any warranty.    *
-*                                                                         *
-\*************************************************************************/
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * main.c
+ * Copyright (C) 2013 Alexandre Filgueira <alexfilgueira@antergos.com>
+ * 
+ * remendo is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * remendo is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <sys/inotify.h>
 #include <limits.h>
