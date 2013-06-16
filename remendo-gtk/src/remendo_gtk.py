9b0de324db9095e130aa61e18a83aad1a03f6e6c
Alexandre Filgueira <faidoc@gmail.com>
2013-06-16 23:24:10 +0200

    Remendo GTK fully working with threading.

--- remendo-gtk/src/remendo_gtk.py
+++ remendo-gtk/src/remendo_gtk.py
@@ -1,6 +1,6 @@
 # -*- Mode: Python; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- #!/usr/bin/python
 #
-# main.py
+# remendo_gtk.py
 # Copyright (C) 2013 Alexandre Filgueira <alexfilgueira@antergos.com>
 # 
 # remendo-gtk is free software: you can redistribute it and/or modify it
@@ -16,19 +16,34 @@
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import Gtk, GdkPixbuf, Gdk, Pango
-import os, sys
+from gi.repository import Gtk, Pango, GObject, Gdk
+import os,sys
 import xml.etree.ElementTree as etree
 from urllib.request import urlopen
 from subprocess import Popen
 from threading import Thread
 
 
-#Comment the first line and uncomment the second before installing
-#or making the tarball (alternatively, use project variables)
 UI_FILE = "remendo_gtk.ui"
 #UI_FILE = "/usr/local/share/remendo_gtk/ui/remendo_gtk.ui"
+DATABASE = '../../remendo/database.db'
 
+class Script(Thread):
+	def __init__(self):
+		self.url_script = ''
+		self.local_script = ''
+		self.running = True
+		Thread.__init__(self)
+	def run(self):
+		script = urlopen(self.url_script)
+		localscript = open(self.local_script, 'wb')
+		localscript.write(script.read())
+		localscript.close()
+		
+		command = ['bash', self.local_script]
+		p = Popen(command)
+		p.communicate()
+		self.running = False
 
 class GUI:
 	
@@ -39,17 +54,15 @@ class GUI:
 		self.builder.connect_signals(self)
 
 		self.window = self.builder.get_object('remendo')
-		self.progress_window = self.builder.get_object('progress_window')
 		self.event_treeview = self.builder.get_object('event_treeview')
-		self.ok_progress = self.builder.get_object('ok_progress')
-		self.ok_progress.hide()
-		self.spinner_progress = self.builder.get_object('spinner_progress')
-		self.progress_info = self.builder.get_object('progress_info')
+		self.event_info = self.builder.get_object('event_info')
+		self.spinner = self.builder.get_object('spinner')
+		self.spinner.hide()
 
 		self.selected_event = ''
 		self.url_script = ''
 		self.local_script = ''
-		
+
 		self.set_event_list()
 
 		self.window.show_all()
@@ -72,7 +85,7 @@ class GUI:
 		self.populate_events(event_liststore)
 
 	def populate_events(self, event_liststore):
-		tree = etree.parse('../../remendo/database.db')
+		tree = etree.parse(DATABASE)
 		root = tree.getroot()
 
 		for event in root.findall('event'):
@@ -89,66 +102,54 @@ class GUI:
 			(ls, iter) = selected.get_selected()
 			if iter:
 				self.selected_event = ls.get_value(iter, 0)
+				self.url_from_selected()
 
 	def on_fixme_button_clicked(self, button):
-		self.url_from_selected()
-		self.progress_window.show()
-		self.spinner_progress.start()
-		self.download_script()
-		self.progress_info.set_label("Fixing now...")
-		script = Thread(target=self.run_script(), args=(self))
-		script.start()
-		self.progress_info.set_label("Marking event as 'Fixed' in database...")
-		self.event_solved()
-		self.progress_info.set_label("My work is done. Good day!")
-		self.spinner_progress.stop()
-		self.ok_progress.show()
+		self.spinner.start()
+		self.s = Script()
+		self.s.local_script = '/tmp/%s.sh' % self.selected_event.replace(" ", "_")
+		self.s.url_script = self.url_script
+		self.s.start()
+		self.run_script()
+		GObject.timeout_add(200, self.run_script)
 		
-
+		
 	def on_manually_button_clicked(self, button):
-		tree = etree.parse('../../remendo/database.db')
+		tree = etree.parse(DATABASE)
 		xpath = './/event[name="%s"]/state' % self.selected_event
 		tree.find(xpath).text = 'Skipped'
-		tree.write('../../remendo/database.db')
+		tree.write(DATABASE)
 		self.set_event_list()
 
 	def url_from_selected(self):
-		tree = etree.parse('../../remendo/database.db')
+		tree = etree.parse(DATABASE)
 		xpath = './/event[name="%s"]/url_script' % self.selected_event
 		self.url_script = tree.find(xpath).text
 
-	def download_script(self):
-		try:
-			self.progress_info.set_label("Downloading script...")
-			self.local_script = '/tmp/%s.sh' % self.selected_event.replace(" ", "_")
-			script = urlopen(self.url_script)
-			localscript = open(self.local_script, 'wb')
-			localscript.write(script.read())
-			localscript.close()
-		except URLError as e:
-			self.progress_info.set_label("Error downloading script")
-
 	def run_script(self):
-		try:
-			command = ['pkexec', 'bash', self.local_script]
-			p = Popen(command)
-			p.communicate()
-		except:
-			self.progress_info.set_label("Script failed: %s" % self.local_script)
-
+		if self.s.running:
+			self.event_info.set_label("Fixing now...")
+		if self.s.running is False:
+			self.event_solved()
+			self.spinner.stop()
+			self.event_info.set_label("My work is done. Have a nice day!")
+			return False
+		return True
+			
 	def event_solved(self):
-		tree = etree.parse('../../remendo/database.db')
+		tree = etree.parse(DATABASE)
 		xpath = './/event[name="%s"]/state' % self.selected_event
 		tree.find(xpath).text = 'Fixed'
-		tree.write('../../remendo/database.db')
+		tree.write(DATABASE)
 		self.set_event_list()
 
-	def on_ok_progress_clicked(self, button):
-		self.progress_window.hide()
 
 def main():
+	GObject.threads_init()
 	app = GUI()
+	Gdk.threads_enter()
 	Gtk.main()
+	Gdk.threads_leave()
 		
 if __name__ == "__main__":
 	sys.exit(main())
